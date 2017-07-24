de32ac6f0da631e981078d0a46166542e157c0b4
piotrkramarz <piotr.m.kramarz@gmail.com>
2017-07-24 20:49:14 +0200

    fixed cookie monster + video added

--- assets/scripts/cookies.js
+++ assets/scripts/cookies.js
@@ -9,7 +9,8 @@ if (!document.cookie.match(/entryDate/)) {
     container.setAttribute('id', 'cookieinfo');
     container.setAttribute('class', 'cookie-alert');
     container.setAttribute('class', 'cookie_monster');
-    container.innerHTML = '<h6>Ta strona wykorzystuje pliki cookie</h6><img src="../assets/images/cookie_monster.png" alt="cookie_monster" class="cookie_monster"/><p>Używamy informacji zapisanych za pomocą plików cookies w celu zapewnienia maksymalnej wygody w korzystaniu z naszego serwisu. Jeżeli wyrażasz zgodę na zapisywanie informacji zawartej w cookies kliknij na x w prawym górnym rogu tej informacji. Jeśli nie wyrażasz zgody, ustawienia dotyczące plików cookies możesz zmienić w swojej przeglądarce.</p>';    // Make closing link
+    container.innerHTML = '<h6>Ta strona wykorzystuje pliki cookie</h6><img src="assets/images/cookie_monster.png" alt="cookie_monster" class="cookie_monster"/><p>Używamy informacji zapisanych za pomocą plików cookies w celu zapewnienia maksymalnej wygody w korzystaniu z naszego serwisu. Jeżeli wyrażasz zgodę na zapisywanie informacji zawartej w cookies kliknij na x w prawym górnym rogu tej informacji. Jeśli nie wyrażasz zgody, ustawienia dotyczące plików cookies możesz zmienić w swojej przeglądarce.</p>';
+    // Make closing link
     var link = document.createElement('a');
     link.setAttribute('href', '#');
     link.setAttribute('title', 'Zamknij');
