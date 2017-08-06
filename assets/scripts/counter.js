6970b2f38996a025eb8f87e3802a3eb4564180ab
piotrkramarz <piotr.m.kramarz@gmail.com>
2017-08-06 22:20:08 +0200

    fix counter to (start function when section 'in view')

--- assets/scripts/counter.js
+++ assets/scripts/counter.js
@@ -2,13 +2,55 @@
  * Created by piotrek on 2017-08-06.
  */
 
-$('.count').each(function () {
-    var $this = $(this);
-    jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-        duration: 2000,
-        easing: 'swing',
-        step: function () {
-            $this.text(Math.ceil(this.Counter));
+var $findme = $('#numbers');
+var exec = false;
+function Scrolled() {
+    $findme.each(function() {
+        var $section = $(this),
+            findmeOffset = $section.offset(),
+            findmeTop = findmeOffset.top,
+            findmeBottom = $section.height() + findmeTop,
+            scrollTop = $(document).scrollTop(),
+            visibleBottom = window.innerHeight,
+            prevVisible = $section.prop('_visible');
+
+        if ((findmeTop > scrollTop + visibleBottom) ||
+            findmeBottom < scrollTop) {
+            visible = false;
+        } else visible = true;
+
+        if (!prevVisible && visible) {
+            if(!exec){
+                $('.count').each(function() {
+                    $(this).prop('Counter', 0).animate({
+                        Counter: $(this).text()
+                    }, {
+                        duration: 2000,
+
+                        step: function(now) {
+                            $(this).text(Math.ceil(now));
+                            exec = true;
+                        }
+                    });
+                });
+            }
         }
+        $section.prop('_visible', visible);
     });
+
+}
+
+function Setup() {
+    var $top = $('#top'),
+        $bottom = $('#bottom');
+
+    $top.height(500);
+    $bottom.height(500);
+
+    $(window).scroll(function() {
+        Scrolled();
+    });
+}
+$(document).ready(function() {
+    Setup();
 });
