7c07bb6cbdcf187c17a2532e8d56403b112ae81f
piotrkramarz <piotr.m.kramarz@gmail.com>
2017-08-07 22:16:04 +0200

    fix number of errors
    *remove unnecessary comments
    *modify button id name
    *remove onclick from html
    *move top-button to left, so its not covering social-media buttons

--- assets/scripts/top-button.js
+++ assets/scripts/top-button.js
@@ -2,16 +2,24 @@
  * Created by piotrkramarz on 06.08.17.
  */
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
+$(document).ready(function () {
+    $("#my-Btn").click(topFunction)
+    {
+
+        window.onscroll = function () {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("my-Btn").style.display = "block";
+            } else {
+                document.getElementById("my-Btn").style.display = "none";
+            }
+        }
     }
-}
-
+});
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera
