dc289aede27a7a5bf0722d6087213825e828d5fd
wojtrawi <wojtrawi@gmail.com>
2017-08-20 13:57:12 +0200

    Optimize JS code.

--- assets/scripts/greeting-card.js
+++ assets/scripts/greeting-card.js
@@ -1,17 +1,33 @@
 $(document).ready(function () {
-    // Add initial greeting card
     var myCanvas = new fabric.Canvas('drawing-area', {
         backgroundColor: 'yellow'
     });
-    myCanvas.setWidth($('.canvas-wrapper').width());
-    myCanvas.setHeight(0.5 * myCanvas.getWidth());
-    canvasContent('Happy Birthday!', 'We wish You\nall the best!', 'assets//images/cookie-monster.png');
 
-    function canvasContent(header, text, imgPath) {
+    var $section = $('#generator');
+    var $wishType = $section.find('input[name="wishes-type"]');
+    var $bgcColor = $section.find('#background-color');
+    var $downloadBtn = $section.find('#download-canvas');
+    var $window = $(window);
+
+    function changeWishes(wishType) {
+        switch (wishType) {
+            case 'traditional':
+                changeContent('Happy Valentines Day!', 'Happy\nValentines Day!', 'assets//images/generator/heart.png');
+                break;
+            case 'funny':
+                changeContent('Have a great pizza day!', 'Happy\npizza day!', 'assets//images/generator/pizza-slice.png');
+                break;
+            case 'custom':
+                changeContent('Have a great celebration of your own!', 'Have a\n happy celebration!', 'assets//images/generator/sun.png');
+                break;
+        }
+    }
+
+    function changeContent(header, text, imgPath) {
         var backgroundColor = myCanvas.backgroundColor;
         myCanvas.clear();
         myCanvas.backgroundColor = backgroundColor;
-        // Add wishes header
+
         var wishesHeader = new fabric.Textbox(header, {
             width: 0.65 * myCanvas.getWidth(),
             left: 0.1 * myCanvas.getWidth(),
@@ -20,8 +36,7 @@ $(document).ready(function () {
             fontFamily: 'Century Gothic',
             textAlign: 'center'
         });
-        myCanvas.add(wishesHeader);
-        // Add wishes content
+
         var wishesText = new fabric.Textbox(text, {
             width: 0.65 * myCanvas.getWidth(),
             left: 0.1 * myCanvas.getWidth(),
@@ -30,8 +45,10 @@ $(document).ready(function () {
             fontFamily: 'Century Gothic',
             textAlign: 'center'
         });
+
+        myCanvas.add(wishesHeader);
         myCanvas.add(wishesText);
-        // Add image
+
         var image;
         fabric.Image.fromURL(imgPath, function (img) {
             image = img;
@@ -41,42 +58,10 @@ $(document).ready(function () {
         });
     }
 
-    // Download greeting card
-    function downloadCanvas() {
-        this.href = myCanvas.toDataURL('image/png');
-    }
-
-    $('#download-canvas').click(downloadCanvas);
-
-    // Change wishes
-    function changeWishes(wishType) {
-        switch (wishType) {
-            case 'traditional':
-                canvasContent('Happy Valentines Day!', 'Happy\nValentines Day!', 'assets//images/generator/heart.png');
-                break;
-            case 'funny':
-                canvasContent('Have a great pizza day!', 'Happy\npizza day!', 'assets//images/generator/pizza-slice.png');
-                break;
-            case 'custom':
-                canvasContent('Have a great celebration of your own!', 'Have a\n happy celebration!', 'assets//images/generator/sun.png');
-                break;
-        }
-    }
-
-    $('input[name="wishes-type"]').click(function () {
-        changeWishes($(this).val());
-    });
-
-    // Change background color
-    $('#background-color').change(function () {
-        myCanvas.setBackgroundColor($(this).val());
-        myCanvas.renderAll();
-    });
-
-    // Canvas resize
-    function scaleCanvas(desiredWidth, actualWidth, canvas) {
+    function scaleContent(desiredWidth, actualWidth, canvas) {
         var scaleMultiplier = desiredWidth / actualWidth;
         var objects = canvas.getObjects();
+
         for (var i in objects) {
             objects[i].scaleX = objects[i].scaleX * scaleMultiplier;
             objects[i].scaleY = objects[i].scaleY * scaleMultiplier;
@@ -84,19 +69,39 @@ $(document).ready(function () {
             objects[i].top = objects[i].top * scaleMultiplier;
             objects[i].setCoords();
         }
+
         canvas.setWidth(desiredWidth);
         canvas.setHeight(0.5 * desiredWidth);
         canvas.renderAll();
         canvas.calcOffset();
     }
 
-    $(window).resize(function () {
+    function downloadContent() {
+        this.href = myCanvas.toDataURL('image/png');
+    }
+
+    $wishType.click(function () {
+        changeWishes($(this).val());
+    });
+
+    $bgcColor.change(function () {
+        myCanvas.setBackgroundColor($(this).val());
+        myCanvas.renderAll();
+    });
+
+    $downloadBtn.click(downloadContent);
+
+    $window.resize(function () {
         var desiredWidth = $('.canvas-wrapper').width();
         var actualWidth = myCanvas.getWidth();
 
         if (actualWidth != desiredWidth) {
-            scaleCanvas(desiredWidth, actualWidth, myCanvas);
+            scaleContent(desiredWidth, actualWidth, myCanvas);
         }
     });
+
+    myCanvas.setWidth($('.canvas-wrapper').width());
+    myCanvas.setHeight(0.5 * myCanvas.getWidth());
+    changeContent('Happy Birthday!', 'We wish You\nall the best!', 'assets//images/cookie-monster.png');
 });
 
