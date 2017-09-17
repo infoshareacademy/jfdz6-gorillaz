43deb42696e506f3dbd20100671c96bdac65aa0c
Michal Czaplicki <mcz.czaplicki@gmail.com>
2017-09-17 13:27:00 +0200

    fix display captchy on mobile phone
    -iphone5!

--- assets/scripts/antybot.js
+++ assets/scripts/antybot.js
@@ -3,3 +3,32 @@ document.getElementById('buttonOnBlock').disabled = true;
 function enableBtn(){
     document.getElementById('buttonOnBlock').disabled = false;
 }
+
+function scaleCaptcha(elementWidth) {
+    // Width of the reCAPTCHA element, in pixels
+    var reCaptchaWidth = 304;
+    // Get the containing element's width
+    var containerWidth = $('.container').width();
+
+    // Only scale the reCAPTCHA if it won't fit
+    // inside the container
+    if(reCaptchaWidth > containerWidth) {
+        // Calculate the scale
+        var captchaScale = containerWidth / reCaptchaWidth;
+        // Apply the transformation
+        $('.g-recaptcha').css({
+            'transform':'scale('+captchaScale+')'
+        });
+    }
+}
+
+$(function() {
+
+    // Initialize scaling
+    scaleCaptcha();
+
+    // Update scaling on window resize
+    // Uses jQuery throttle plugin to limit strain on the browser
+    $(window).resize( $.throttle( 100, scaleCaptcha ) );
+
+});
