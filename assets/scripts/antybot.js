f4f918a2e8a20543f621ef8e64f1d1a82921767f
Michal Czaplicki <mcz.czaplicki@gmail.com>
2017-09-17 13:39:01 +0200

    up

--- assets/scripts/antybot.js
+++ assets/scripts/antybot.js
@@ -5,17 +5,15 @@ function enableBtn(){
 }
 
 function scaleCaptcha(elementWidth) {
-    // Width of the reCAPTCHA element, in pixels
+
     var reCaptchaWidth = 304;
-    // Get the containing element's width
     var containerWidth = $('.container').width();
 
-    // Only scale the reCAPTCHA if it won't fit
-    // inside the container
+
     if(reCaptchaWidth > containerWidth) {
-        // Calculate the scale
+
         var captchaScale = containerWidth / reCaptchaWidth;
-        // Apply the transformation
+
         $('.g-recaptcha').css({
             'transform':'scale('+captchaScale+')'
         });
@@ -24,11 +22,8 @@ function scaleCaptcha(elementWidth) {
 
 $(function() {
 
-    // Initialize scaling
     scaleCaptcha();
 
-    // Update scaling on window resize
-    // Uses jQuery throttle plugin to limit strain on the browser
     $(window).resize( $.throttle( 100, scaleCaptcha ) );
 
 });
