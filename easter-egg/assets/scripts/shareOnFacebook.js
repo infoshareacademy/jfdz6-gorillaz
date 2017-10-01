80798401222b4fea7d32e80867ae17ec42ab948d
Michal Czaplicki <mcz.czaplicki@gmail.com>
2017-10-01 14:02:55 +0200

    lot of things

---
+++ easter-egg/assets/scripts/shareOnFacebook.js
@@ -0,0 +1,30 @@
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id))
+        return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '409742669131720',  // Change appId 409742669131720 with your Facebook Application ID
+        status: true,
+        xfbml: true,
+        cookie: true
+    });
+};
+
+$(document).ready(function() {
+    $('.shareFacebook').click(function() {
+        FB.ui({
+            method: 'feed',
+            name: 'Manoj Yadav',
+            link: 'http://www.manojyadav.co.in/',
+            picture: 'https://www.gravatar.com/avatar/119a8e2d668922c32083445b01189d67',
+            description: 'Manoj Yadav a PHP Developer from Mumbai, India'
+        });
+    });
+});
