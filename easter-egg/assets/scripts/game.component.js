f7c01ca9cc852db1e0a22bfc647b47ed2dc4275b
wojtrawi <wojtrawi@gmail.com>
2017-10-01 13:09:45 +0200

    Add logged user tracking.
    - if user is already logged in, after page refresh or revisit
    there is no need to sign in again

--- easter-egg/assets/scripts/game.component.js
+++ easter-egg/assets/scripts/game.component.js
@@ -2,7 +2,18 @@ $(document).ready(function () {
     const httpsService = HttpsService('https://white-frog-7647.getsandbox.com');
     const timerService = TimerService();
     const statisticsService = StatisticsService(httpsService, timerService);
-    const loginModalComponent = LoginModalComponent(httpsService, statisticsService, timerService);
+
+    if (!localStorage.token) {
+        const loginModalComponent = LoginModalComponent(httpsService, statisticsService, timerService);
+    } else {
+        httpsService.post('/users/verify', {token: localStorage.token})
+            .then((userBestScore) => {
+                statisticsService.changedBestScore.next(userBestScore.bestScore);
+                statisticsService.retrievedUser.next(userBestScore.username);
+                timerService.startTimer();
+            });
+    }
+
     const dashboardComponent = DashboardComponent(statisticsService);
     const boardComponent = BoardComponent(statisticsService);
     const succesModalComponent = SuccessModalComponent(httpsService, statisticsService);
