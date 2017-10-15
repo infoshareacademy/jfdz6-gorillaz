f5e325fd166c8460c2cd6298345df3a5baaf8690
Michal Czaplicki <mcz.czaplicki@gmail.com>
2017-10-15 14:10:49 +0200

    facebook cnd

--- easter-egg/assets/scripts/shareOnFacebook.js
+++ easter-egg/assets/scripts/shareOnFacebook.js
@@ -1,30 +1,34 @@
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id))
-        return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+// (function(d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id))
+//         return;
+//     js = d.createElement(s);
+//     js.id = id;
+//     js.src = "//connect.facebook.net/en_US/all.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+//
+// window.fbAsyncInit = function() {
+//     FB.init({
+//         appId: '1641729335901592',
+//         status: true,
+//         xfbml: true,
+//         cookie: true
+//     });
+// };
+//
+// FB.ui({
+//     method: 'feed',
+//     link: 'https://developers.facebook.com/docs/',
+//     caption: 'An example caption',
+// }, function(response){});
 
-window.fbAsyncInit = function() {
-    FB.init({
-        appId: '409742669131720',  // Change appId 409742669131720 with your Facebook Application ID
-        status: true,
-        xfbml: true,
-        cookie: true
-    });
-};
 
-$(document).ready(function() {
-    $('.shareFacebook').click(function() {
-        FB.ui({
-            method: 'feed',
-            name: 'Manoj Yadav',
-            link: 'http://www.manojyadav.co.in/',
-            picture: 'https://www.gravatar.com/avatar/119a8e2d668922c32083445b01189d67',
-            description: 'Manoj Yadav a PHP Developer from Mumbai, India'
-        });
-    });
-});
+    // <script>(function(d, s, id) {
+    //     var js, fjs = d.getElementsByTagName(s)[0];
+    //     if (d.getElementById(id)) return;
+    //     js = d.createElement(s); js.id = id;
+    //     js.src = "//connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v2.10&appId=1641729335901592";
+    //     fjs.parentNode.insertBefore(js, fjs);
+    // }(document, 'script', 'facebook-jssdk'));
+
